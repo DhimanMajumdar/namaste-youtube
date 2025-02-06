9c59a06b0aa46c904a718a91a7b61aaf533fa6d6
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-02-06 15:16:30 +0530

    Sidebar Toggling feature done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { Provider } from 'react-redux';
 import Body from './components/Body';
 import Head from './components/Head';
 import './index.css'
+import store from './utils/store';
 
 
 function App() {
   return (
+    <Provider store={store}>
     <div>
       <Head/>
       <Body/>
     </div>
+    </Provider>
   );
 }
 
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { use } from "react";
 import logo from "../assets/logo.png";
 import menuIcon from "../assets/menu.jpg";
 import userIcon from '../assets/userIcon.png'
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  const dispatch=useDispatch();
+  const toggleMenuHandler=()=>{
+    dispatch(toggleMenu());
+  }
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
-        <img className="h-8" alt="menu" src={menuIcon} />
-        <img className="h-8 mx-2" src={logo} alt="VibeStream Logo" />
+        <img onClick={()=>toggleMenuHandler()} className="h-8 cursor-pointer" alt="menu" src={menuIcon} />
+        <a><img className="h-8 mx-2" src={logo} alt="VibeStream Logo" /></a>
       </div>
       <div className="col-span-10 px-10">
         <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text"/>
